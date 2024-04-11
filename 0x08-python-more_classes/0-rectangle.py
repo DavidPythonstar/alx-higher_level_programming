a3a7aa4b43fec21ce2af8da920fdd5e43fdd77c0
DavidPythonstar <davidpainzug@gmail.com>
2024-04-11 03:49:25 -0700

    task 0

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+"""Defines a Rectangle class."""
 
-"""Define a Rectangle class"""
 
 class Rectangle:
-    """Representation of a rectangle"""
+    """Represention of  a rectangle."""
     pass
 
